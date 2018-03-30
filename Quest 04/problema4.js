e3dc46b6af1a8d8e04eb3de664a3d497dd14cad7
Anderson Mendes <>
2018-03-30 15:45:08 -0400

    ok
    ok

--- Quest 04/problema4.js
+++ Quest 04/problema4.js
@@ -1,25 +1,16 @@
 var fs = require('fs');
 var math = require('mathjs');
-var listAdjacency = [];
-var matrixAdjacency = math.matrix();
-
-//-------------------------------------------------------
-//search matrixadjacency
+var ListEdges= [];
+var ListAux = [];
+//var matrixAdjacency = math.matrix();
 //// NOTE: Questao incompleta. Se tiver mais tempo termino:
+//-------------------------------------------------------
+//search matrixadjacency;
+//search list edges;
 
-class graph {
-  constructor() {
-    this.left = null;
-    this.right = null;
-    this.center = null
-  }
-  setValue(value){
-    this.value = value;
-  }
-  getValue() {
-    return this.value;
-  }
-}
+
+
+
 
 fs.readFile('./triangulo.txt', 'utf-8', function(err, data){
   if(err){
@@ -29,7 +20,7 @@ fs.readFile('./triangulo.txt', 'utf-8', function(err, data){
     var lines = data.split(/\r?\n/);
     lines.forEach(function(line){
        flag = line.replace( /\s/g,',');
-       listAdjacency.push(flag);
+       ListAux.push(flag);
     })
     
 })
