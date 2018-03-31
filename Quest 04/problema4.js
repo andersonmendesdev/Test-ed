d2057ec1af71e2924c77ed22fe994ed479a00cbc
Anderson Mendes <>
2018-03-30 20:06:59 -0400

    ok
    ok

--- Quest 04/problema4.js
+++ Quest 04/problema4.js
@@ -2,6 +2,8 @@ var fs = require('fs');
 var math = require('mathjs');
 var ListEdges= [];
 var ListAux = [];
+var ListFile = [];
+var ListNumbers = [];
 //var matrixAdjacency = math.matrix();
 //// NOTE: Questao incompleta. Se tiver mais tempo termino:
 //-------------------------------------------------------
@@ -11,16 +13,32 @@ var ListAux = [];
 
 
 
-
-fs.readFile('./triangulo.txt', 'utf-8', function(err, data){
+var aux;
+var i = 0;
+fs.readFile('./triangulos.txt', 'utf-8', function(err, data){
   if(err){
     console.log("Error:",err);
     return;
   }
     var lines = data.split(/\r?\n/);
     lines.forEach(function(line){
-       flag = line.replace( /\s/g,',');
-       ListAux.push(flag);
+       flag = line.replace( /\s/g,'');
+       aux = flag.toString(flag);
+       for(i=0; i<aux.length ;i+=2){
+        ListAux.push(parseInt(aux.substring(i,i+2)));
+        ListNumbers.push(parseInt(aux.substring(i,i+2)));
+      }
+      ListFile.push(ListAux);
+      ListAux = [];
+            
     })
-    
+    console.log(ListFile);
+    console.log(ListNumbers);
+    //var value = ListFile.shift();
+   // console.log(value.shift());
+    //var value02 = ListFile.shift();
+    //console.log(value02.length);
+    //console.log(value02.shift());
+    //console.log(value02.shift());
+    //console.log(value+value02);
 })
