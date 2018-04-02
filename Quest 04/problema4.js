c6380aa7c1b41a55654767ed14e742f13801f124
Anderson Mendes <37872453+andersonmendesdev@users.noreply.github.com>
2018-04-01 21:35:31 -0400

    ok
    ok

--- Quest 04/problema4.js
+++ Quest 04/problema4.js
@@ -3,8 +3,9 @@ var math = require('mathjs');
 var ListEdges= [];
 var ListAux = [];
 var ListFile = [];
-var ListNumbersOne = [];
-var ListNumbersTwo = [];
+//const ListComp;
+//var ListNumbersOne = [];
+//var ListNumbersTwo = [];
 //var matrixAdjacency = math.matrix();
 //// NOTE: Questao incompleta. Se tiver mais tempo termino:
 //-------------------------------------------------------
@@ -14,8 +15,8 @@ var ListNumbersTwo = [];
 
 
 
-var aux;
-var i = 0;
+
+
 fs.readFile('./triangulos.txt', 'utf-8', function(err, data){
   if(err){
     console.log("Error:",err);
@@ -25,21 +26,57 @@ fs.readFile('./triangulos.txt', 'utf-8', function(err, data){
     lines.forEach(function(line){
        flag = line.replace( /\s/g,'');
        aux = flag.toString(flag);
-       for(i=0; i<aux.length ;i+=2){
+       for(var i=0; i<aux.length ;i+=2){
         ListAux.push(parseInt(aux.substring(i,i+2)));
-        ListNumbersOne.push(parseInt(aux.substring(i,i+2)));
-        ListNumbersTwo.push(parseInt(aux.substring(i,i+2)));
+        //ListNumbersOne.push(parseInt(aux.substring(i,i+2)));
+        //ListNumbersTwo.push(parseInt(aux.substring(i,i+2)));
       }
+      //console.log(ListAux);
       ListFile.push(ListAux);
       ListAux = [];
-            
+
     })
-    console.log(ListFile);
-    console.log(ListNumbersOne);
-    console.log('\n',ListNumbersTwo);
-    var control = 0;
-    while(control !== 100){
-
-    }
-
+    FormGrp(ListFile);
 })
+
+function FormGrp(ListComp) {
+    var ListOne = [];
+    var ListTwo = [];
+    var count = 0;
+    var Control;
+    var graph = [];
+    //var ListComp = [[59],[73,41],[52,40,9],[26,53,6,34],[10,51,87,86,81]];
+
+    Control = 4;
+    console.log(ListComp);
+
+    while(Control != 0){
+          ListOne = ListComp[count];
+          ListTwo = ListComp[count+1];
+          for(var i=0; i<ListOne.length; i++){
+                for(var j=0; j<ListTwo.length; j++){
+                  if(j === 2){
+                      ListTwo.shift();
+                      break;
+                    }
+                    graph.push([ListOne[i],ListTwo[j]]);
+                }
+          }
+          count++;
+          Control--;
+    }
+        console.log('\n',ListComp);
+}
+
+function searchGrp(){
+var total = 0;
+var control = 0;
+var Listarest = [[59,73],[59,41],[73,52],[73,40],[41,40],[41,9],[52,26],[52,53],[40,53],[40,6],[9,6],[9,34];
+var Listfinigsh = [26,53,6,34];
+
+      while(control != 0){
+          
+
+
+      }
+}
