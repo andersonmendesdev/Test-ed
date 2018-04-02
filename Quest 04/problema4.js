bf084e45c6a8342edcf3e04d768051b4c2dc07a1
Anderson Mendes <37872453+andersonmendesdev@users.noreply.github.com>
2018-04-01 23:20:41 -0400

    ok
    ok

--- Quest 04/problema4.js
+++ Quest 04/problema4.js
@@ -47,39 +47,8 @@ function  Graphlevel(ListLevel){
     indice++
     control--
   }
-  console.log('\n',ListLevel[indice-1]);
   for(var k=0;k<levellist.length;k++){
     total += levellist[k];
   }
   console.log('Valor do triangulo '+ total);
 }
-
-/*
-function FormGrp(ListComp) {
-    var ListOne = [];
-    var ListTwo = [];
-    var count = 0;
-    var Control;
-    var graph = [];
-
-    Control = ListComp.length;
-    //console.log(ListComp);
-
-    while(Control != 1){
-          ListOne = ListComp[count].slice();
-          ListTwo = ListComp[count+1].slice();
-          for(var i=0; i<ListOne.length; i++){
-                for(var j=0; j<ListTwo.length; j++){
-                  if(j === 2){
-                      ListTwo.shift();
-                      break;
-                    }
-                    graph.push([ListOne[i],ListTwo[j]]);
-                }
-          }
-          count++;
-          Control--;
-    }
-        //console.log('\n',graph);
-}
-*/
