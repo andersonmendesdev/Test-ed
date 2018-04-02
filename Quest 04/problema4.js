aa40b8702735635beaebaeadd7a6700a692f4c3b
Anderson Mendes <37872453+andersonmendesdev@users.noreply.github.com>
2018-04-02 02:03:56 -0400

    ok
    ok

--- Quest 04/problema4.js
+++ Quest 04/problema4.js
@@ -6,35 +6,34 @@ var ListFile = [];
 //-------------------------------------------------------
 //search list edges;
 
-init();
+init();// inicizaliar
+
+function init(){
+  var list = [];
+  var listGrp = [];
+  list = rdfile();
+  listGrp = FormGrp(list);
+  moveArray(listGrp);
+}
 
 function rdfile(){
   try {  
-      var data = fs.readFileSync('./triangulo.txt', 'utf8');
-           var lines = data.split(/\r?\n/);
-           lines.forEach(function(line){
-           flag = line.replace( /\s/g,'');
-           aux = flag.toString(flag);
+        var data = fs.readFileSync('./triangulo.txt', 'utf8');
+        var lines = data.split(/\r?\n/);
+        lines.forEach(function(line){
+            flag = line.replace( /\s/g,'');
+            aux = flag.toString(flag);
             for(var i=0; i<aux.length ;i+=2){
-           ListAux.push(parseInt(aux.substring(i,i+2)));
+                ListAux.push(parseInt(aux.substring(i,i+2)));
             }
-           ListFile.push(ListAux);
-           ListAux = [];
+            ListFile.push(ListAux);
+            ListAux = [];
        })
-       //console.log(ListFile);
-       return ListFile;   
+        return ListFile;   
       } catch(e) {
           console.log('Error:', e.stack);
       }
-  }
-  
-  function init(){
-      var list = [];
-      var listGrp = [];
-      list = rdfile();
-      listGrp = FormGrp(list);
-      moveArray(listGrp);
-  }
+}
 
 
 function FormGrp(ListComp) {
