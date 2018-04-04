3efbcf16f312c5a9c5978017024a99ce4b087059
Anderson Mendes <anderson_mendes88@hotmail.com>
2018-04-03 20:17:12 -0400

    ok
    ok

--- Quest 04/problema4.js
+++ Quest 04/problema4.js
@@ -8,16 +8,18 @@ init();// inicizaliar
 
 function init(){
   var file = [];
-  var edgesgraph = [];
+  var GraphEgdes = [];
   file = read_files();
-  edgesgraph = graphEdges(file);
-  verifyRoutes(edgesgraph);
+  GraphEgdes = createGraph(file);
+  verifyEdges(GraphEgdes);
+  //verifyEdges(createGraph(read_files()));
 }
 
 function read_files(){
   try {  
         var ListAux = [];
         var data = fs.readFileSync('./triangulo.txt', 'utf8');
+        //var data = fs.readFileSync('./triangulos.txt', 'utf8');
         var lines = data.split(/\r?\n/);
         lines.forEach(function(line){
             flag = line.replace( /\s/g,'');
@@ -34,7 +36,7 @@ function read_files(){
       }
 }
 
-function graphEdges(ListComp) {
+function createGraph(ListComp) {
   var ListOne = [];
   var ListTwo = [];
   var count = 0;
@@ -62,41 +64,40 @@ function graphEdges(ListComp) {
   return graph;
 }
 
-function verifyRoutes(ListArray){
-  var ListMax = [];
+function verifyEdges(ListArray){
+  var ListRoute = [];
   var vectorOne = [];
   var vectorTwo = [];
   var control = ListFile.length - 1;
   var indice = 0;
   var soma = 0;
   vectorOne = ListArray[indice].slice();
-  ListMax.push(vectorOne[0]);
+  ListRoute.push(vectorOne[0]);
   while(control != 0){
         vectorOne = ListArray[indice].slice();
         vectorTwo = ListArray[indice+1].slice();
 
         if(vectorOne[1] > vectorTwo[1]){
-           ListMax.push(vectorOne[1]);
-           indice = positionList(ListArray,vectorOne[1],(vectorOne[2]+1));
+           ListRoute.push(vectorOne[1]);
+           indice = positionEdges(ListArray,vectorOne[1],(vectorOne[2]+1));
         }
         else {
-          ListMax.push(vectorTwo[1]);
-          indice = positionList(ListArray,vectorTwo[1],(vectorTwo[2]+1));
+          ListRoute.push(vectorTwo[1]);
+          indice = positionEdges(ListArray,vectorTwo[1],(vectorTwo[2]+1));
         }
         control--;
   }
-  for(var b=0;b<ListMax.length;b++){
-      soma += ListMax[b];
+  for(var b=0;b<ListRoute.length;b++){
+      soma += ListRoute[b];
   }
-  console.log('Valor triangulo ',soma);
+  console.log('Valor triangulo',soma);
 }
 
-function positionList(ListArr,value,level){
+function positionEdges(ListArr,value,level){
   var Arr = [];
   for(var i=0;i<ListArr.length;i++){
     Arr = ListArr[i].slice();
-    if(Arr[0] === value & Arr[2]=== level)
-    {
+    if(Arr[0] === value & Arr[2]=== level){
       return i;
     }
   }
