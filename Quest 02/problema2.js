6c838e171b6715ef9be5a18d830c4fec1cb71639
andersonmendesdev <37872453+andersonmendesdev@users.noreply.github.com>
2018-07-20 13:39:09 -0400

    refatorado
    refatorado

--- Quest 02/problema2.js
+++ Quest 02/problema2.js
@@ -1,35 +1,23 @@
 const fs = require('fs')
+const data = fs.readFileSync('./lista_palavras_desordenada.txt', 'utf8').split(/\r?\n/)
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-init()
-function init(){
-  
-  console.log(read_file())
-  // NOTE: O valor encontrado caso a posição inicial de 'a' seja 1 é 21947;
-  // NOTE: O valor encontrado caso a posição inicial de 'a' seja 0 é 21867, Resposta do teste.
 
-}
-function read_file(){
-  try {  
-        let data = fs.readFileSync('./lista_palavras_desordenada.txt', 'utf8')
-        let lines = data.split(/\r?\n/);
-        let Words
-        let alphabet = 'abcdefghijklmnopqrstuvwxyz'
-        let countLetters = 0
-        let countWord = 0
-        lines.forEach( line =>{
-          Words = line.toString()
-          for(let i = 0;i < Words.length;i++){
-            countLetters += (alphabet.indexOf(Words[i])); //posição inicial do alfabeto sendo 0; 21867
-            //countLetters += (alphabet.indexOf(Words[i])+1); //posição incial do alfabeto sendo 1; 21947
-          }
-          if(countLetters%5 == 0){
-            countWord++
-          }
-          countLetters = 0
-       })
-        return countWord
-
-      } catch(e) {
-          console.log('Error:', e.stack)
+const lerPalavras = (lines) => {        
+  let countLetters = 0
+  let countWord = 0
+  lines.forEach( line =>{
+    Words = line.toString()
+      for(let i = 0;i < Words.length;i++){
+         countLetters += (alphabet.indexOf(Words[i])); //posição inicial do alfabeto sendo 0; 21867
+         //countLetters += (alphabet.indexOf(Words[i])+1); //posição incial do alfabeto sendo 1; 21947
       }
+      if(countLetters%5 == 0){
+        countWord++
+      }
+      countLetters = 0
+  })
+  return countWord
 }
+
+console.log(lerPalavras(data))
